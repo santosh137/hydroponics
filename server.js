6e979e44965d981f855f3a4ae8a87d47a5d1d011
Dany <admin@DANYS-MacBook-Air.local>
2020-02-01 22:48:10 +0530

    sec commit

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.use(bodyParser.json())
 
 // define a simple route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+    res.json({"message": "Hydro Hustlers"});
 });
 
 // listen for requests
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is listening on port 3000");
 });
